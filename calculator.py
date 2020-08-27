bcf4310c933a04b6f3461a32bf7d5b7d7f51cc05
madargreg <56037178+madargreg@users.noreply.github.com>
2020-08-27 19:27:10 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 =float(input("write your first num:"))
+num3 =input("write an operator: ")
+num2 =float(input("write your second num:"))
+
+if num3 == "*":
+        print(num1 * num2)
+elif num3 == "/":
+        print(num1 / num2)
+elif num3 == "+":
+        print(num1 + num2)
+else:
+        print(num1 - num2)
+
